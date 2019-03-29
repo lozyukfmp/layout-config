56801b3cbe339d931c5565bf6c24ab472aac7fd4
Andrei_Rahozik <andrei.rahozik@netcracker.com>
2019-03-29 12:14:21 +0300

    init

--- client/src/app/services/preferences.service.ts
+++ client/src/app/services/preferences.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {TailorPage} from "../models/TailorPage";
+import {Preferences} from "../models/Preferences";
 import {environment} from "../../environments/environment";
 import {DataBaseService} from "./dataBase.service";
 import {MatSnackBar} from "@angular/material";
@@ -8,7 +8,7 @@ import {MatSnackBar} from "@angular/material";
 @Injectable({
   providedIn: 'root'
 })
-export class PreferencesService extends DataBaseService<TailorPage> {
+export class PreferencesService extends DataBaseService<Preferences> {
 
   constructor(http: HttpClient, snackBar: MatSnackBar) {
     super(http, snackBar, `${environment.baseApiUrl}/preferences`);
