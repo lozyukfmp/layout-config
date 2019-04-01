0b56091c55143306762ff6c7f0a3a76a92b502cf
dzse0118 <dzmitry.seliutsin@netcracker.com>
2019-04-01 15:14:19 +0300

    create search for delete fragments instances

--- server/controllers/layoutController.js
+++ server/controllers/layoutController.js
@@ -1,4 +1,6 @@
 const Layout = require('../models/Layout');
+const Preferences = require('../models/Preferences');
+
 
 module.exports.getAll = async function (req, res) {
     try {
@@ -35,12 +37,20 @@ module.exports.add = async function (req, res) {
 
 module.exports.update = async function (req, res) {
     try {
-        const layout = await Layout.findOneAndUpdate(
+
+        let layout = await Layout.findById(req.params.layoutId);
+        const updatedLayout = await Layout.findOneAndUpdate(
             {_id: req.params.layoutId},
             {$set: req.body},
             {new: true}
         );
-        res.status(201).json(layout);
+
+        const oldFragmentsInstances = getFragmentsInstances(layout.toObject());
+        const newFragmentsInstances = getFragmentsInstances(req.body);
+        const instancesForDelete = oldFragmentsInstances.filter(fragment => !newFragmentsInstances.includes(fragment));
+        instancesForDelete.forEach(async fragmentInstance => await Preferences.remove({fragmentInstanceId: fragmentInstance}));
+
+        res.status(201).json(updatedLayout);
     } catch (e) {
         onError(res, e)
     }
@@ -53,6 +63,17 @@ module.exports.remove = async function (req, res) {
     } catch (e) {
         onError(res, e)
     }
+};
+
+const getFragmentsInstances = layout => {
+    return layout.structure.rows
+        .reduce((a, b) => a.concat(b), [])
+        .map(row => row.columns)
+        .reduce((a, b) => a.concat(b), [])
+        .map(column => column.fragments)
+        .reduce((a, b) => a.concat(b), [])
+        .map(fragment => fragment.instanceId)
+        .reduce((a, b) => a.concat(b), []);
 };
 
 function onError(res, error) {
