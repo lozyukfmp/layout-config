22e808e4364137f5de07a78f67a2e7e170a06f45
arla0217 <artemlozyuk@gmail.com>
2019-04-05 16:59:15 +0300

    reactive style services.

--- client/src/app/services/fragments.service.ts
+++ client/src/app/services/fragments.service.ts
@@ -4,7 +4,7 @@ import {FragmentSchema} from '../models/FragmentSchema';
 import {environment} from '../../environments/environment';
 import {MatSnackBar} from '@angular/material';
 import {DataBaseService} from './dataBase.service';
-import {Observable} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 import {PortalService} from './portal.service';
 import {switchMap} from 'rxjs/internal/operators';
 
@@ -13,10 +13,19 @@ import {switchMap} from 'rxjs/internal/operators';
 })
 export class FragmentsService extends DataBaseService<FragmentSchema> {
 
+  private fragment = new BehaviorSubject<FragmentSchema>();
+  private fragment$ = this.fragment.asObservable;
+  private fragments = new BehaviorSubject<FragmentSchema[]>();
+  private fragments$ = this.fragments.asObservable;
+
   constructor(private http: HttpClient,
               public snackBar: MatSnackBar,
               private portalService: PortalService) {
     super(http, snackBar, `${environment.baseApiUrl}/fragments`);
+  }
+
+  public setSelectedFragment(_id: string) {
+    this.getById(_id).subscribe(result => this.fragment.next(result));
   }
 
   public fetch(): Observable<FragmentSchema[]> {
